def3314700a9538905c86d8e0f1dbf6481d67e65
jasonbolton <jasonbolton.m@gmail.com>
2018-02-27 13:34:58 -0700

    added many comments, consolidation

--- choose_song.py
+++ choose_song.py
@@ -1,5 +1,7 @@
 class ChooseSong:
-
+    """"this class holds the song reference list and
+    the current playlist. it handles updating the order
+    given new songs and picking the next song."""
     def __init__(self, reference_list):
         self._reference_list = reference_list
         self._play_list = []
@@ -11,11 +13,17 @@ class ChooseSong:
         return self._play_list
 
     def update_current_order(self, new_song_list):
+        """given a new list of song arrivals, if the
+        song is in the reference song list, it is added
+        to the end of the playlist"""
         for song in new_song_list:
             if song in self._reference_list:
                 self._play_list.append(song)
 
     def pick_next_song(self):
+        """this method takes the first entry
+        of the playlist, removes it, then returns
+        it to played."""
         next_song = self._play_list[0]
         print(next_song)
         print(self._play_list)
--- phone_simulator.py
+++ phone_simulator.py
@@ -1,32 +1,45 @@
+REFERENCE_LIST_DECODER_CHAR = "*"
+PLAY_LIST_DECODER_CHAR = ":"
+
+
 from iota import *
 import random
 
 def main():
+    """this is the main program for the phone simulation
+    function of the jukebox. this program reads in encoded
+    playlist and reference list information from the tangle,
+    decodes and displays that information, and is able to vote
+    for songs based on that information."""
     spent_transactions = {}
+    # address to monitor
     address = 'BJAKMIXYBLAAPKLBCGHELQCSKOMZLSAYLOHDBOYRJFQJIHBWCCCIUBVLQKYPTHWVBQWTZM9JGMAPFUCBCBCSRTKJLY'
+    # a proof of work node is required
     node = 'https://iotanode.us:443'
     api = Iota(node)
-    
+
+    # reads in the encoded messages from the tangle
     encoded_message_list = get_tangle_info(spent_transactions, api, address)
 
-    
-    decoded_message = decode_reference_list_message(encoded_message_list)
-    reference_list = make_reference_list(decoded_message)
-
-    
+    # decodes the reference list, processes, displays
+    decoded_message = decode_message_list(encoded_message_list, REFERENCE_LIST_DECODER_CHAR)
+    reference_list = make_message_list(decoded_message, REFERENCE_LIST_DECODER_CHAR)
     display_song_list(reference_list)
     print(reference_list)
-
-    decoded_message = decode_play_list_message(encoded_message_list)
-    play_list = make_play_list(decoded_message)
+    
+    # decodes the play list, processes, displays
+    decoded_message = decode_message_list(encoded_message_list, PLAY_LIST_DECODER_CHAR)
+    play_list = make_message_list(decoded_message, PLAY_LIST_DECODER_CHAR)
     display_song_list(play_list)
     print(play_list)
     
-    #sends a song out to the tangle. comment out when not desired.
+    # sends a song out to the tangle. comment out when not desired.
     #song = reference_list[1]
     #vote_for_song(song, address, api)
 
 def get_tangle_info(spent_transactions, api, address):
+    """reads in transactions from the tangle. if the tag has already
+    been read, it is not processed. the messages are returned in a list"""
     message_list = []
     transaction_dict = api.find_transactions(bundles=None, \
                         addresses=[address], tags=None, approvees=None)
@@ -35,34 +48,24 @@ def get_tangle_info(spent_transactions, api, address):
         transaction = Transaction.from_tryte_string(trytes)
         if transaction.tag not in spent_transactions:
             message_list.append(transaction.signature_message_fragment)
+            spent_transactions[transaction.tag] = 0
     return message_list
 
-def decode_reference_list_message(message_list): 
+def decode_message_list(message_list, decoder_char):
+    """decodes and returns the first message found that
+    contains the decoder_char sequence"""
     for message in message_list:
         message = message.decode()
         print(message)
         for i in range(len(message)):
-            if message[i] == "*":
-                return message[i:]
-
-def decode_play_list_message(message_list): 
-    for message in message_list:
-        message = message.decode()
-        print(message)
-        for i in range(len(message)):
-            if message[i] == ":":
+            if message[i] == decoder_char and \
+               message[i+1] == decoder_char:
                 return message[i:]
             
-def make_reference_list(message):
-    song_split = message.split("*")
-    clean_song_split = []
-    for i in range(len(song_split)):
-        if len(song_split[i]) > 0:
-            clean_song_split.append(song_split[i])
-    return clean_song_split
-
-def make_play_list(message):
-    song_split = message.split(":")
+def make_message_list(message, decoder_char):
+    """returns a song list by splitting a song string
+    containing songs split by a decoder_char"""
+    song_split = message.split(decoder_char)
     clean_song_split = []
     for i in range(len(song_split)):
         if len(song_split[i]) > 0:
@@ -70,19 +73,24 @@ def make_play_list(message):
     return clean_song_split
             
 def display_song_list(song_list):
+    """given a list of songs, print out a formatted
+    number: song_name string"""
     for i in range(len(song_list)):
         print(str(i) + ":", song_list[i])
 
 def vote_for_song(song, address, api):
+    """this function is used for encoding and voting
+    for a song to play. a song name is enclosed by
+    two ?? and encoded to trytes. this is the transaction
+    messsage. a random tag is generated. the transaction
+    is sent to the monitored address."""
     random_tag = ""
     song = "??" + song + "??"
+    song = TryteString.from_unicode(song)
     print(song)
     for i in range(27):
         rand_char = chr(random.randint(80, 90))
         random_tag += rand_char
-    print(song)
-    song = TryteString.from_unicode(song)
-    print(song)
     if len(random_tag) > 27:
         excess = len(random_tag) - 27
         random_tag = random_tag[excess:]
